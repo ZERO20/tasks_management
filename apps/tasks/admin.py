7fc54d9788a10f7995f75cf18ff60413275405bc
Edgar de la Cruz <edgar.cruz@xepelin.com>
2022-11-23 15:32:54 -0600

    feat: added Task Admin

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.tasks.models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    """Task Admin Class"""
+    list_display = ('name', 'user', 'status', 'created_at',)
+    list_filter = ('created_at',)
+    readonly_fields = ('created_at', 'updated_at',)
+    search_fields = ('name', 'description', 'user__username', 'user__email',)
+    fields = ('name', 'description', 'user', 'created_at', 'updated_at',)
+    ordering = ('-created_at',)
